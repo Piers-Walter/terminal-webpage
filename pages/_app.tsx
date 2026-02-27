2832c96cdb9f50f9ff00ba6491641dc4cc17457b
Piers Walter <pierswalter3@googlemail.com>
2026-02-27 09:39:39 +0000

    Add tracking & title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,19 @@ import "@/styles/globals.css";
 import DesktopApp from "@/utils/DesktopApp";
 import type { AppProps } from "next/app";
 import { useMemo, useState } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [runningApps, setRunningApps] = useState<DesktopApp[]>([]);
   const [activeUUID, setActiveUUID] = useState<string>("");
   const fileSystem = useMemo(() => new FakeFS(), []);
   return (
-    <AppContext.Provider value={{ runningApps, setRunningApps, activeUUID, setActiveUUID, fileSystem }}>
+    <AppContext.Provider
+      value={{ runningApps, setRunningApps, activeUUID, setActiveUUID, fileSystem }}
+    >
+      <Head>
+        <title>OtterOS</title>
+      </Head>
       <Component {...pageProps} />
     </AppContext.Provider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
